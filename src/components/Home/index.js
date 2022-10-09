f6fd519473489a7e9b76a600a898433740bded55
Kenzy <shoouxian@gmail.com>
2022-10-09 02:42:24 -0400

    1/3 done

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm <img src={LogoTitle} alt="developer"/> Kenzy <br/> Software Developer </h1> 
+                <h2>
+                    
+                </h2>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
